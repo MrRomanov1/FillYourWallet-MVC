2fa0413049094666e0a0d7b2f244ddcae58afb16
Piotr Romanczak <piotr.romanczak.programista@gmail.com>
2020-10-04 20:21:33 +0200

    Added database connection config

---
+++ App/Config.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+class Config
+{
+
+    const DB_HOST = 'localhost';
+
+    const DB_NAME = 'fill_your_wallet';
+
+    const DB_USER = 'root';
+   
+    const DB_PASSWORD = '';
+    
+    const SHOW_ERRORS = true;
+}
